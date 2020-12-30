06470acbd545b0c958e959cf6144b614519794ea
Hamzah <hamzah.ahmed1@education.nsw.gov.au>
2020-12-30 14:16:21 +1100

    Make typing game

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from "react"
+import useWordGame from "./hooks/useWordGame"
 
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
+  const {
+    textBoxRef,
+    handleChange,
+    text,
+    isTimeRunning,
+    timeRemaining,
+    startGame,
+    wordCount
+  } = useWordGame(20)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+      <div>
+        <h1>How fast do you type?</h1>
+        <textarea
+            ref={textBoxRef}
+            onChange={handleChange}
+            value={text}
+            disabled={!isTimeRunning}
+        />
+        <h4>Time remaining: {timeRemaining}</h4>
+        <button
+            onClick={startGame}
+            disabled={isTimeRunning}
+        >
+          Start
+        </button>
+        <h1>Word count: {wordCount}</h1>
+      </div>
+  )
 }
 
-export default App;
+export default App
